73243887b135a83bb9d8b76fc19f58d1a0ec45aa
FNogueira2023 <f.nogueira2022@outlook.com>
2022-09-28 13:36:40 -0300

    Inicio funcionalidad de ejs y json

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,14 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const jsonData = fs.readFileSync(path.join(__dirname, '../models/data/lastNews.json'),
+    { encoding: 'utf8' });
+
+const lastNewsData = JSON.parse(jsonData);
+
+console.log(jsonData);
+console.log(lastNewsData);
+
+
 const controller = {
-    index: (req,res) => {
-        res.render('index');
+    index: (req, res) => {
+        res.render('index', {lastNewsData});
     },
-    article: (req,res) => {
+    article: (req, res) => {
         res.render('article');
     },
-    review: (req,res) => {
+    review: (req, res) => {
         res.render('review');
     }
-    
+
 };
 
 
