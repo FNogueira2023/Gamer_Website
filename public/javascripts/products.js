b13d6a84b14c418d2a6b3c4c5cfeee246afba3d6
FNogueira2023 <f.nogueira2022@outlook.com>
2022-11-22 17:50:56 -0300

    Carrousel dinamico

---
+++ public/javascripts/products.js
@@ -0,0 +1,59 @@
+window.onload = () => {
+
+    const track = document.querySelector('.track');
+
+    fetch('https://dummyjson.com/products')
+        .then(res => res.json())
+        .then(data => {
+            let products = data.products;
+            console.log(products);
+
+            products.forEach(product => {
+                const cardContainer = document.createElement('div');
+                cardContainer.setAttribute('class', 'card-container');
+
+                const card = document.createElement('div');
+                card.setAttribute('class', 'card');
+              
+
+                const image = document.createElement('img');
+                image.src = product.images[0];
+                image.setAttribute('class', 'card-image');
+
+                const info = document.createElement('p');
+                info.textContent = product.title;
+                info.setAttribute('class','card-info');
+
+                
+                const a = document.createElement('a');
+                const link = document.createTextNode(info.textContent);                   
+                a.appendChild(link);    
+                a.appendChil                       
+                a.title = info.textContent;                
+                a.href = `https://www.amazon.com/s?k=${product.title}`;
+                
+               
+
+
+
+
+
+
+
+                track.appendChild(cardContainer);
+                cardContainer.appendChild(card);
+                card.appendChild(image);
+               
+                card.appendChild(a);
+                a.appendChild(info);
+
+
+
+            });
+
+
+        });
+
+
+
+}
