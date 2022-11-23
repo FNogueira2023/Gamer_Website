d31685558b5adcf0f8090265f4cd3b01b1203981
FNogueira2023 <f.nogueira2022@outlook.com>
2022-11-23 17:07:46 -0300

    Actualizacion del login para sequelize

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,22 +5,6 @@ const { validationResult } = require('express-validator');
 const db = require('../database/models')
 
 
-
-
-function findAll() {
-    const jsonData = fs.readFileSync(path.join(__dirname, '../data/users.json'));
-    const data = JSON.parse(jsonData);
-    return data
-}
-
-
-function writeFile(data) {
-    const dataString = JSON.stringify(data, null, 4);
-    fs.writeFileSync(path.join(__dirname, '../data/users.json'), dataString);
-}
-
-
-
 const controller = {
     create: (req, res) => {
         res.render('loginRegister');
@@ -34,17 +18,19 @@ const controller = {
                     errors: errors.array(),
                     old: req.body
                 });
-        } else {            
+        } else {
+            let hash = bcryptjs.hashSync(req.body.pswd, 10);
+
             db.Users.create({
                 email: req.body.email,
-                pswd: req.body.pswd,
+                pswd: hash,
                 userName: req.body.userName
             });
 
             res.redirect('/');
         }
     },
-    login: (req, res) => {
+    login: async (req, res) => {
         let errors = validationResult(req);
 
         if (!errors.isEmpty()) {
@@ -54,15 +40,15 @@ const controller = {
                     old: req.body
                 });
         } else {
-            const data = findAll()
 
-            for (let i = 0; i < data.length; i++) {
-                if (data[i].email == req.body.email) {
-                    if (bcryptjs.compareSync(req.body.pswd, data[i].pswd)) {
-                        var loggedUser = data[i];
-                        break;
-                    }
-                }
+            const { email, pswd } = req.body;
+            let loggedUser;
+
+            const user = await db.Users.findOne({ where: { email: email } });
+            const checkPassword = await bcryptjs.compare(pswd, user.pswd);
+
+            if (checkPassword) {
+                loggedUser = user;
             }
 
             if (loggedUser == undefined) {
@@ -74,7 +60,6 @@ const controller = {
                     });
             }
 
-
             req.session.user = loggedUser;
 
             res.cookie('rememberUser', loggedUser.email, {
@@ -82,7 +67,6 @@ const controller = {
             })
 
             return res.render('loginRegister');
-
         }
     }
 }
